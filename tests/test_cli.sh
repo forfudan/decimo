f8b6bdf5eca480ded53bda0e07757a5708a0caf2
ZHU Yuhao <dr.yuhao.zhu@outlook.com>
2026-04-07 23:03:21 +0200

    [float][gmp] Implement most of the methods for `BigFloat` (#191)

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -2,5 +2,5 @@
 set -e  # Exit immediately if any command fails
 
 for f in tests/cli/*.mojo; do
-    pixi run mojo run -I src -I src/cli -D ASSERT=all "$f"
+    pixi run mojo run -I src -I src/cli -D ASSERT=all -debug-level=line-tables "$f"
 done
